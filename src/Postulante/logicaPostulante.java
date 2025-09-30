c41f7fa8cadb15faf89cd2a64f1d4131de79552a
ALvarexGuitar <159080814+ALvarexGuitar@users.noreply.github.com>
2025-09-29 21:31:46 -0400

    Se modifico el metodo habilitar de la clsse Administrador2 para integrar la asignacion de aula directamente al habilitar, se agrego una nueva tabla en la BD asignacionaula para guardar esta informacion. Se crearon los metodos asignarAula y verInfo para mostrar la informacion del examen y demas datos.

--- src/Postulante/logicaPostulante.java
+++ src/Postulante/logicaPostulante.java
@@ -57,6 +57,82 @@ public class logicaPostulante {
 
         return false;
     }
+    
+    
+    
+    public static boolean asignacionAula(int idPostulante, String apellido, String fecha, String hora, String lugar) {
+    String aula;
+    char inicial = Character.toUpperCase(apellido.charAt(0));
+    if (inicial >= 'A' && inicial <= 'M') {
+        aula = "Aula 101";
+    } else {
+        aula = "Aula 102";
+    }
 
+    String sql = "INSERT INTO asignacionAula (id_postulante, fecha, hora, lugar, aula) VALUES (?, ?, ?, ?, ?)";
+
+    try (Connection con = ConexionDB.getConnection();
+         PreparedStatement ps = con.prepareStatement(sql)) {
+
+        ps.setInt(1, idPostulante);
+        ps.setDate(2, java.sql.Date.valueOf(fecha)); 
+        ps.setTime(3, java.sql.Time.valueOf(hora)); 
+        ps.setString(4, lugar);
+        ps.setString(5, aula);
+
+        int filas = ps.executeUpdate();
+        return filas > 0;
+
+    } catch (SQLException e) {
+        e.printStackTrace();
+        return false;
+    }
+}
+
+    
+    public static String verInformacionExamen(String cedula) {
+        
+    String info = "";
+    String sqlPostulante = "SELECT id, nombre, apellidos FROM postulante WHERE cedula = ?";
+    String sqlExamen = "SELECT fecha, hora, lugar, aula FROM asignacionAula WHERE id_postulante = ?";
+
+    try (Connection con = ConexionDB.getConnection();
+         PreparedStatement psPostulante = con.prepareStatement(sqlPostulante)) {
+
+        psPostulante.setString(1, cedula);
+        ResultSet rsPostulante = psPostulante.executeQuery();
+
+        if (rsPostulante.next()) {
+            int idPostulante = rsPostulante.getInt("id");
+            String nombre = rsPostulante.getString("nombre");
+            String apellidos = rsPostulante.getString("apellidos");
+
+            try (PreparedStatement psExamen = con.prepareStatement(sqlExamen)) {
+                psExamen.setInt(1, idPostulante);
+                ResultSet rsExamen = psExamen.executeQuery();
+
+                if (rsExamen.next()) {
+                    info = "Postulante: " + nombre + " " + apellidos +
+                           "\nFecha: " + rsExamen.getDate("fecha") +
+                           "\nHora: " + rsExamen.getTime("hora") +
+                           "\nLugar: " + rsExamen.getString("lugar") +
+                           "\nAula: " + rsExamen.getString("aula");
+                } else {
+                    info = "No se encontró información de examen para " + nombre + " " + apellidos;
+                }
+            }
+
+        } else {
+            info = "Cédula no registrada.";
+        }
+
+    } catch (SQLException e) {
+        e.printStackTrace();
+        info = "Error al consultar la información.";
+    }
+
+    return info;
+}
+  
 }
 
